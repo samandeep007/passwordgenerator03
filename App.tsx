300f0acb67cd52e71037b9981d5b5ebf9a516766
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-03 15:22:56 -0600

    feat: Add form validation for password length
    - Add form validation using Yup library
    - Define a schema for password length with minimum and maximum character limits
    - Display error message if password length is not within the specified range
    
    Refactor App component and remove unnecessary code
    
    - Remove unused import statements and styles
    
    Update npm dependencies to include yup@1.4.0

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+//Form validation
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+                     .min(4, "Should be min of 4 characters")
+                     .max(16, "Should be a max of 16 characters")
+                     .required('Length is required')
+})
+
 export default function App(): JSX.Element {
   return (
     <View>
@@ -9,4 +19,6 @@ export default function App(): JSX.Element {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+})
